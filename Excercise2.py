165c3f483c00e80eada870081f2283347ce0d801
elephantatech <vivekm.mistry@gmail.com>
2019-04-16 09:30:48 -0400

    fixed problems list and changed to dictionary for nodetree

--- Excercise2.py
+++ Excercise2.py
@@ -12,7 +12,8 @@ class Node:
         else:
             return False
 
-nodetree = [Node(1,0),Node(2,1),Node(3,1),Node(4,2), Node(5,2), Node(6,3), Node(7,3), Node(8,4), Node(9,4)]
+nodetree = { 1 : Node(1,0), 2 : Node(2,1), 3 : Node(3,1), 4 : Node(4,2),
+             5 : Node(5,2), 6 : Node(6,3), 7 : Node(7,3), 8: Node(8,4), 9: Node(9,4)}
 
 
 def lca(node1, node2):
@@ -20,24 +21,26 @@ def lca(node1, node2):
     Function takes 2 parameters of nodes and return common parent
     :param node1:
     :param node2:
-    :return: parent_value
+    :return: common parent value on both nodes
 
     this function will take time to run if the nodetree is big as the function core runs recursively.
     Therefore the further apart the nodes on the node tree the longer it takes to find the parent.
     So other mechanisms need to be added to calculate how far apart the nodes are and how to get the common parent faster
     in a logical manner instead of a recursive manner.
     """
+    parent_tree1 = get_all_parents(node1)
+    parent_tree2 = get_all_parents(node2)
+    common_parents = set(parent_tree1).intersection(parent_tree2)
 
-    parent_value = confirm_parent(node1.parent, node2.parent)
+    return max(common_parents)
 
-    while parent_value == None:
-        if node1.isRoot() or node2.isRoot():
-            break
-        node1 = nodetree[node1.parent]
-        node2 = nodetree[node2.parent]
-        parent_value = confirm_parent(node1.parent, node2.parent)
+def get_all_parents(node):
+    parent_list = []
+    while node.parent > 0:
+        parent_list.append(nodetree[node.parent].value)
+        node = nodetree[node.parent]
 
-    return parent_value
+    return parent_list
 
 
 def confirm_parent(num1, num2):
@@ -47,6 +50,5 @@ def confirm_parent(num1, num2):
         return None
 
 
-
-print(lca(nodetree[1],nodetree[2]))
-print(lca(nodetree[7],nodetree[8]))
+print(lca(nodetree[8],nodetree[9]))
+print(lca(nodetree[6],nodetree[8]))
